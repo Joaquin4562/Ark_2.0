c4e05f959a76bb7e42d1e0754465d160c6baf225
Joaquin4562 <martinez.hernandez.17087@itsmante.edu.mx>
2019-10-31 22:15:04 -0600

    Fragments perfil y acerca de y borrado de fragmens inecesarios

--- app/src/main/java/com/example/ark_versiondos/Busqueda.java
+++ app/src/main/java/com/example/ark_versiondos/Busqueda.java
@@ -4,109 +4,40 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.ark_versiondos.ui.home.HomeViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Busqueda.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Busqueda#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Busqueda extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public Busqueda() {
-        // Required empty public constructor
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_busqueda, container, false);
+        RecyclerView recyclerView = root.findViewById(R.id.lista_animales);
+        recyclerView.setAdapter(new RecyclerViewAdapter(obtenerRazas()));
+        return root;
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Busqueda.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Busqueda newInstance(String param1, String param2) {
-        Busqueda fragment = new Busqueda();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    public List<razaVacas> obtenerRazas(){
+        List<razaVacas> raza = new ArrayList<>();
+        raza.add(new razaVacas("Brahma","mexico",R.drawable.vaca,R.drawable.milk));
+        raza.add(new razaVacas("Simbrah","mexico",R.drawable.vaca,R.drawable.steak));
+        raza.add(new razaVacas("Pardo Suiza","mexico",R.drawable.vaca,R.drawable.milk));
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_busqueda, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        return raza;
     }
 }
---
+++ app/src/main/java/com/example/ark_versiondos/ui/home/PerfilViewModel.java
@@ -0,0 +1,7 @@
+package com.example.ark_versiondos.ui.home;
+
+import androidx.lifecycle.ViewModel;
+
+public class PerfilViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
