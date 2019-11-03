554f86b8313d063d3e35be5f8be0cfa49da15944
Joaquin4562 <martinez.hernandez.17087@itsmante.edu.mx>
2019-11-02 19:44:30 -0600

    completado la clase buscar

--- app/src/main/java/com/example/ark_versiondos/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/ark_versiondos/RecyclerViewAdapter.java
@@ -5,7 +5,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -13,13 +12,14 @@ import java.util.List;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
     List<razaVacas> listaRazas;
+
     public RecyclerViewAdapter(List<razaVacas> listaRazas) {
         this.listaRazas = listaRazas;
     }
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.elemento_lista,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.elemento_lista,null,false);
         ViewHolder viewHolder = new ViewHolder(view);
         return viewHolder;
     }
--- app/src/main/java/com/example/ark_versiondos/ui/home/Busqueda.java
+++ app/src/main/java/com/example/ark_versiondos/ui/home/Busqueda.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -12,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 
 import com.example.ark_versiondos.R;
+import com.example.ark_versiondos.RecyclerViewAdapter;
 import com.example.ark_versiondos.razaVacas;
 
 import java.util.ArrayList;
@@ -20,19 +22,31 @@ import java.util.List;
 
 public class Busqueda extends Fragment {
     RecyclerView recyclerView;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_busqueda, container, false);
-       // recyclerView = root.findViewById(R.id.lista_animales);
-        //recyclerView.setAdapter(new RecyclerViewAdapter(obtenerRazas()));
+        recyclerView = root.findViewById(R.id.lista_animales);
+
+        recyclerView.setAdapter(new RecyclerViewAdapter(obtenerRazas()));
+        recyclerView.setLayoutManager(new LinearLayoutManager(root.getContext()));
         return root;
     }
 
     public List<razaVacas> obtenerRazas(){
         List<razaVacas> raza = new ArrayList<>();
-        raza.add(new razaVacas("Brahma","mexico",R.drawable.vaca,R.drawable.milk));
-        raza.add(new razaVacas("Simbrah","mexico",R.drawable.vaca,R.drawable.steak));
-        raza.add(new razaVacas("Pardo Suiza","mexico",R.drawable.vaca,R.drawable.milk));
+        raza.add(new razaVacas("Brahma","mexico",R.drawable.vaca,R.drawable.ic_carne));
+        raza.add(new razaVacas("Simbrah","mexico",R.drawable.vaca,R.drawable.ic_botella_de_leche));
+        raza.add(new razaVacas("Pardo Suiza","mexico",R.drawable.vaca,R.drawable.ic_carne));
+        raza.add(new razaVacas("Brahma","mexico",R.drawable.vaca,R.drawable.ic_botella_de_leche));
+        raza.add(new razaVacas("Simbrah","mexico",R.drawable.vaca,R.drawable.ic_carne));
+        raza.add(new razaVacas("Pardo Suiza","mexico",R.drawable.vaca,R.drawable.ic_botella_de_leche));
+        raza.add(new razaVacas("Brahma","mexico",R.drawable.vaca,R.drawable.ic_carne));
+        raza.add(new razaVacas("Simbrah","mexico",R.drawable.vaca,R.drawable.ic_botella_de_leche));
+        raza.add(new razaVacas("Pardo Suiza","mexico",R.drawable.vaca,R.drawable.ic_carne));
+        raza.add(new razaVacas("Brahma","mexico",R.drawable.vaca,R.drawable.ic_botella_de_leche));
+        raza.add(new razaVacas("Simbrah","mexico",R.drawable.vaca,R.drawable.ic_carne));
+        raza.add(new razaVacas("Pardo Suiza","mexico",R.drawable.vaca,R.drawable.ic_botella_de_leche));
         return raza;
     }
 }
